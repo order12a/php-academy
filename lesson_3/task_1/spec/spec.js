e6a0b7c3af4a77d84f2c35453455e6618c4a739a
order12a <vladimir.uroshlev@gmail.com>
2017-02-04 22:56:52 +0200

    Lesson_3 first task finished

---
+++ lesson_3/task_1/spec/spec.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var ArrReverse = require('../ArrReverse');
+
+describe('reverse of array elements tests', function () {
+    var arrReverse;
+
+    before(function () {
+        arrReverse = new ArrReverse();
+    });
+
+    it('should reverse array', function () {
+        arrReverse.reverse();
+        var arr = [1, 2, 3, 4, 5];
+        var expectedArr = [5, 4, 3, 2, 1];
+
+        expect(arr.reverseElements()).to.deep.equal(expectedArr);
+    });
+})
