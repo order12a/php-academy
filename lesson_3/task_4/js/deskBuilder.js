d46a6150fb7af2e8d6da449917770761c1bfdc57
order12a <vladimir.uroshlev@gmail.com>
2017-02-07 23:04:36 +0200

    Task 4 ready

--- lesson_3/task_4/js/deskBuilder.js
+++ lesson_3/task_4/js/deskBuilder.js
@@ -15,50 +15,26 @@ var buildChessDesk = function () {
     var container = document.getElementsByClassName('container')[0];
 
     for(var i = 0; i < 8; i++){
-        console.log(container);
+        var row = container.appendChild(document.createElement('div'))
         for (var y = 0; y < 8; y++){
             if(marker % 2 === 1 || marker === 1){
                 console.log('We create white element');
                 var whiteBox = document.createElement('span');
                 whiteBox.setAttribute('class', 'white');
-                container.appendChild(whiteBox);
+                row.appendChild(whiteBox);
                 marker++;
             }else {
                 console.log('We create black element');
                 var blackBox = document.createElement('span');
                 blackBox.setAttribute('class', 'black');
-                container.appendChild(blackBox);
+                row.appendChild(blackBox);
                 marker++;
             }
         }
         marker++;
-        container.appendChild(document.createElement('br'));
     }
 };
 
-function build() {
-    var marker = 1;
-    var container = document.getElementsByClassName('container')[0];
 
-    for(var i = 0; i < 10; i++){
-        for (var y = 0; y < 10; y++){
-            if(marker % 2 === 1 || marker === 1){
-                console.log('We create white element');
-                var whiteBox = document.createElement('span');
-                whiteBox.className('white');
-                container.appendChild(whiteBox);
-                marker++;
-            }else {
-                console.log('We create black element');
-                var blackBox = document.createElement('span');
-                blackBox.className('black');
-                container.appendChild(blackBox);
-                marker++;
-            }
-        }
-
-        container.appendChild(document.createElement('br'));
-    }
-}
 
 
